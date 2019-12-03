8d0ab55ea4e2899aa00e22a77fa521397f878859
Daria <daria359@gmail.com>
2019-12-03 14:28:08 +0100

    Install basic packages

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => console.log("Server Up and running"));
+
+
+
+
+// In terminal type "npm start" to start nodemon
