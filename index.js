80e9daa96dc3f45b292ec1126e1a8fbd82fa8597
Daria <daria359@gmail.com>
2019-12-03 15:30:41 +0100

    Connect to database

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+//connection to db
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true}, () => {
+    console.log("Connected to db!");
+
+    app.listen(3000, () => console.log("Server Up and running"));
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
-app.listen(3000, () => console.log("Server Up and running"));
-
-
 
 
 // In terminal type "npm start" to start nodemon
