24cf58ec2d4ee59d10134ff44d412a0c47a637ac
Olimpia Kwiecień <kwiecien@zoho.com>
2019-12-04 17:38:28 +0100

    add post method

--- index.js
+++ index.js
@@ -19,4 +19,9 @@ app.get('/', (req, res) => {
 // Urlencoded will allow us to extract the data from the form by adding her to the body property of the request
 app.use(express.urlencoded({ extended: true }));
 
+// POST METHOD
+app.post('/',(req, res) => {
+    console.log(req.body);
+});
+
 // In terminal type "npm start" to start nodemon
