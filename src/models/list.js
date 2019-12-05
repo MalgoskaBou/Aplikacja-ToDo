afe6cb9ec1ebf86f6f04fc8a0714641734bffaab
Magdalena Szczęch <magdalenaszczech96@gmail.com>
2019-12-05 15:13:16 +0100

    add user module

--- src/models/list.js
+++ src/models/list.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const listSchema = new mongoose.Schema({
-    user_id: {},
-    list_name: {}
+    user_id: {
+        type: String,
+        required: true,
+    },
+    list_name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 30
+    }
 });
 
 module.exports = mongoose.model('List', listSchema);
