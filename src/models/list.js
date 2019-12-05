10c484b741fbb987bf65706c2b8fecfae7b37215
Magdalena Szczęch <magdalenaszczech96@gmail.com>
2019-12-05 16:20:40 +0100

    add the rest modules

--- src/models/list.js
+++ src/models/list.js
@@ -4,12 +4,14 @@ const listSchema = new mongoose.Schema({
     user_id: {
         type: String,
         required: true,
+        minlength: 3,
+        maxlength: 1024,
     },
     list_name: {
         type: String,
         required: true,
         minlength: 1,
-        maxlength: 30
+        maxlength: 30,
     }
 });
 
--- src/models/task.js
+++ src/models/task.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    user_id: {},
-    list_id: {},
-    task_name: {},
-    checked: {}
+    user_id: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 1024,
+    },
+    list_id: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+    },
+    task_name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+    checked: {
+        type: Boolean,
+    }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
