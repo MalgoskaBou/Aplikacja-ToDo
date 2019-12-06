1d8289fb898d309a6f588b7937b617ef03fbc635
magdalena <magdalenaszczech96@gmail.com>
2019-12-06 22:22:42 +0100

    auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function(req, res, next) {
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).send('Acces denied. No token provided.')
+};
