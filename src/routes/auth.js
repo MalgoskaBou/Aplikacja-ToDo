ab5b658b4ec3a5b4216841b2cfe4ec010b2c2468
magdalena <magdalenaszczech96@gmail.com>
2019-12-08 15:57:12 +0100

    Add auth route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,3 +6,28 @@ const {userSchema} = require('../models/user');
 const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
+
+router.post("/", async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await userSchema.findOne({ login: req.body.login });
+    if (!user) return res.status(400).send('Invalid login or password.');
+  
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid login or password.');
+
+    const token = user.generateAuthToken();
+    res.send(token);
+  });
+
+  function validate (req) {
+      const schema = {
+          login: 
+          password: 
+      };
+
+      return 
+  }
+
+  module.exports = router;
