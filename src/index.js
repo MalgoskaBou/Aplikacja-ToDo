414d0a479263d616164d70975978b431af6b8f47
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2019-12-09 12:55:53 +0100

    Add user to database
    // Add user to database

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ const cors = require('cors')
 const mongoose = require("mongoose");
 require('./db/db');
 
+// Module user
+const users = require("./routes/users");
+
 dotenv.config();
 
 const port = process.env.PORT;
+
+// Connection with server
+mongoose.connect('mongodb://localhost:3000/api')
+    .then(() => console.log('Now connected to MongoDB!'))
+    .catch(err => console.error('Something went wrong', err));
 
 const app = express();
 const router = express.Router();
@@ -17,11 +25,20 @@ app.use(cors());
 app.use(express.json());
 app.use("/static", express.static("public"));
 // Urlencoded will allow us to extract the data from the form by adding her to the body property of the request
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Define rout for users
+app.use('/api/users', users);
 
 // Launch server
 app.listen(port, err => {
-    if (err) {throw err;} else {console.log(`Server running on port: ${port}`)}
+    if (err) {
+        throw err;
+    } else {
+        console.log(`Server running on port: ${port}`)
+    }
 });
 
 
---
+++ src/routes/users.js
@@ -0,0 +1,33 @@
+// Add user to database 
+const User = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    // First Validate the request
+    const {
+        error
+    } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Check if this user already exists
+    const user = await User.findOne({
+        login: req.body.login
+    });
+    if (user) {
+        return res.status(400).send('That user already exists!');
+    } else {
+        // Insert the new user if they do not exist yet
+        user = new User({
+            login: req.body.login,
+            password: req.body.password
+        });
+
+        await user.save();
+        res.send(user);
+    }
+});
+
+module.exports = router;
