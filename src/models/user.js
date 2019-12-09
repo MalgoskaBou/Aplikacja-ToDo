a9c1a8579e8155f5baf9b14b75b286be37193ab4
Daria <daria359@gmail.com>
2019-12-09 10:49:48 +0100

    Refactor authorization in User model

--- src/models/user.js
+++ src/models/user.js
@@ -20,16 +20,37 @@ const userSchema = new mongoose.Schema({
     }],
 });
 
-userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign(
-      {
-        _id: this._id,
-        login: this.name,
-      },
-      process.env.JWT_PRIVATE_KEY
-    );
+userSchema.pre('save', async function (next) {
+    // Hash the password before saving the user model
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    };
+    next();
+})
+
+// https://medium.com/swlh/jwt-authentication-authorization-in-nodejs-express-mongodb-rest-apis-2019-ad14ec818122
+userSchema.methods.generateAuthToken = async function() {
+    // Generate an auth token for the user
+    const user = this;
+    const token = jwt.sign({_id: user._id}, process.env.JWT_PRIVATE_KEY);
+    user.tokens = user.tokens.concat({token});
+    await user.save();
     return token;
-  };
+}
+
+userSchema.statics.findByCredentials = async (login, password) => {
+    // Search for a user by email and password.
+    const user = await User.findOne({ login } );
+    if (!user) {
+        throw new Error({ error: 'Invalid login!' })
+    };
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+        throw new Error({ error: 'Invalid password!' })
+    };
+    return user;
+}
 
 const User = mongoose.model("User", userSchema);
 
