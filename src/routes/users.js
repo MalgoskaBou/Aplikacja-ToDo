2bd96ab58d9bb890ab9f6f8c43ef85d6bcca31f1
magdalena <magdalenaszczech96@gmail.com>
2019-12-11 14:15:30 +0100

    Refactor the code

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,21 +4,18 @@ const express = require("express");
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    // First Validate the request
     const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    // Check if this user already exists
     let user = await User.findOne({ login: req.body.login });
     if (user) return res.status(400).send('That user already exists!');
-    // Insert the new user if they do not exist yet
+
     user = new User({
         login: req.body.login,
         password: req.body.password
     });
     await user.save();
     res.send(user);
-    }
 });
 
 module.exports = router;
