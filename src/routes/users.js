a9cf4251f3125be6295a94c3d8fc54583f8bac0b
magdalena <magdalenaszczech96@gmail.com>
2019-12-11 14:35:13 +0100

    Add salt

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@
 const User = require("../models/user");
 const express = require("express");
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
@@ -12,6 +13,8 @@ router.post('/', async (req, res) => {
     if (user) return res.status(400).send('That user already exists!');
 
     user = new User(_.pick(req.body, ['login', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     await user.save();
 
     res.send(_.pick(user, ['_id', 'login']));
