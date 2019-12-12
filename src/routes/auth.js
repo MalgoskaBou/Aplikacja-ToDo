645c5febedc9182ac2f034d3f15c036b3e0fa54e
Daria <daria359@gmail.com>
2019-12-12 04:07:25 +0100

    Add comments

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-// const validator = require('validator'); ? const joi = require('joi'); ?
 const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const {userSchema} = require('../models/user');
 const express = require('express');
-const auth = require('../middleware/auth');
+// const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post("/", async (req, res) => {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,25 +1,33 @@
-// const jwt = require('jsonwebtoken');
-// const config = require('config');
+// const jwt = require("jsonwebtoken");
+// const config = require("config");
 const User = require("../models/user");
 const express = require("express");
-const _ = require('lodash');
-const bcrypt = require('bcrypt');
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    // Register new user
     const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ login: req.body.login });
-    if (user) return res.status(400).send('That user already exists!');
+    let user = await User.findOne({ "login": req.body.login });
+    if (user) return res.status(400).send("That user already exists!");
 
-    user = new User(_.pick(req.body, ['login', 'password']));
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+    user = new User(_.pick(req.body, ["login", "password"]));
+
+/* To jest niepotrzebne bo w modelu usera jest dodana funkcja "pre-save" która z automatu hashuje przed zapisaniem
+    // const salt = await bcrypt.genSalt(10);
+    // user.password = await bcrypt.hash(user.password, salt);
+*/
     await user.save();
 
+
+/* To chyba nie zadziała bo w modelu usera na devie nie ma takiej funkcji
     const token = user.generateAuthToken();
     res.header('x-auth-token', token).send(_.pick(user, ['_id', 'login']));
+*/
+
 });
 
 module.exports = router;
