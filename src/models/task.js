796331d5624deb6eee6c04a546b7a6c77b8e1537
Crazy Sheep <iv.palka@gmail.com>
2019-12-14 09:56:55 +0100

    update validation

--- src/models/task.js
+++ src/models/task.js
@@ -1,26 +1,41 @@
 const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 
 const taskSchema = new mongoose.Schema({
   _userID: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "User"
   },
   _listID: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "List",
+    ref: "List"
   },
   name: {
     type: String,
     required: true,
     minlength: 3,
-    maxlength: 50,
+    maxlength: 50
   },
   checked: {
     type: Boolean,
-    default: false,
+    default: false
   }
 });
 
 const Task = mongoose.model("Task", taskSchema);
+Joi.objectId = require("joi-objectid")(Joi);
+function validateTask(task) {
+  const schema = Joi.object({
+    _userID: Joi.objectId(),
+    _listID: Joi.objectId(),
+    name: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    checked: Joi.boolean()
+  });
+  return schema.validate(task);
+}
 
 module.exports = Task;
+exports.validate = validateTask;
