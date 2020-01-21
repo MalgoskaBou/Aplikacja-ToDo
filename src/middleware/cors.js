a6d5b7e4747bb95769f2688a9bede42876331bc8
Daria <daria359@gmail.com>
2020-01-21 23:31:14 +0100

    Add PATCH method in CORS config

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,5 +1,5 @@
 const corsMiddleware = function(req, res, next) {
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, PATCH");
     res.header("Access-Control-Allow-Headers", "Content-Type, x-auth-token");
     next();
   };
